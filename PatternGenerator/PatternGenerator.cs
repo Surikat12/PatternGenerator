d3448f29e899e1c95b985aa28ff21407bd9a7f50
Surikat12 <Surikat12@yandex.ru>
2022-03-03 18:41:06 +0300

    Реализовал функционал класса PatternGenerator

--- PatternGenerator/PatternGenerator.cs
+++ PatternGenerator/PatternGenerator.cs
@@ -1,23 +1,44 @@
-﻿using System;
+﻿using System.IO;
 
 namespace PatternGeneratorApp
 {
     public class PatternGenerator
     {
+        private readonly string _compositePath;
+        private readonly string _visitorPath;
+
         public PatternGenerator(string compositePath, string visitorPath)
         {
-            throw new NotImplementedException();
+            _compositePath = compositePath;
+            _visitorPath = visitorPath;
+        }
+
+        private static string ReadFromFile(string fileName)
+        {
+            using (var sr = new StreamReader(fileName))
+            {
+                return sr.ReadToEnd();
+            }
         }
 
         public string GenerateComposite(string componentName, string leafName, string namespaceName)
         {
-            throw new NotImplementedException();
+            var template = ReadFromFile(_compositePath);
+            template = template.Replace("{0}", componentName);
+            template = template.Replace("{1}", leafName);
+            template = template.Replace("{2}", namespaceName);
+            return template;
         }
 
         public string GenerateVisitor(string iComponentName, string iVisitorName, string visitorName,
             string namespaceName)
         {
-            throw new NotImplementedException();
+            var template = ReadFromFile(_visitorPath);
+            template = template.Replace("{0}", iComponentName);
+            template = template.Replace("{1}", iVisitorName);
+            template = template.Replace("{2}", visitorName);
+            template = template.Replace("{3}", namespaceName);
+            return template;
         }
     }
 }
